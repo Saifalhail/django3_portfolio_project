9e8dafeadac23959f4e814d00212043cf002803d
Saif Alhail <saifalhail@gmail.com>
2021-12-29 12:11:10 +0300

    First Commit

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Project_blog
+
+admin.site.register(Project_blog)
+
+
---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Project_blog
+
+
+
+def all_blogs(request):
+    projects_blogs = Project_blog.objects.all()
+    return render(request, 'blog/all_blogs.html', {'projects_blogs':projects_blogs})
+
+def detail (request, blog_id):
+    blog = get_object_or_404(Project_blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog':blog})
